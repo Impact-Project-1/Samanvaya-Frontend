6983a7b5ac6a34506fa3f6b541476dc4687b9329
rohitrn_ <rohitrajeev05@gmail.com>
2026-05-23 13:42:58 +0530

    connected vendors in backend to frontend display

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Matches all requests starting with /api/
+        destination: 'http://localhost:8000/api/:path*', // Forwards to your backend
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,13 +3,38 @@ import HeroSection from "@/components/HeroSection";
 import CategoryGrid from "@/components/CategoryGrid";
 import VendorList from "@/components/VendorList";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(
+    "http://localhost:8000/api/v1/vendors/filter/",
+    {
+      cache: "no-store",
+    }
+  );
+
+  const result = await response.json();
+
+  const data = result.data;
+
+  // SPLIT FEATURED + NORMAL
+  const featured = data.filter(
+    (vendor: { is_featured: boolean }) =>
+      vendor.is_featured
+  );
+
+  const normal = data.filter(
+    (vendor: { is_featured: boolean }) =>
+      !vendor.is_featured
+  );
+
   return (
     <main>
       <Navbar />
       <HeroSection />
       <CategoryGrid />
-      <VendorList />
+      <VendorList
+        title="Featured Vendors"
+        vendors={featured}
+      />
     </main>
   );
 }
